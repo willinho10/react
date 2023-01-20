d32c96ea5354a3e41c5025d0805105079f5f9ae0
Willinho10 <william.baudu@free.fr>
2023-01-20 16:25:17 +0100

    weather tp noté

--- src/App.js
+++ src/App.js
@@ -1,81 +1,104 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-class App extends Component {
+function App() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    state = {
-        tasks: [
-            { name: 'Tâche 1', done: false },
-            { name: 'Tâche 2', done: true },
-            { name: 'Tâche 3', done: false },
-        ],
-    };
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-    newTask = '';
-
-
-    handleChange(taskName) {
-        const { tasks } = this.state;
-        const newTasks = tasks.map(task => {
-            if (task.name === taskName) {
-                task.done = !task.done;
-            }
-            return task;
+    async function fetchData() {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            const geoResponse = await axios.get(`https://izudztw6jk.execute-api.eu-west-1.amazonaws.com/Prod/geo?lon=${lon}&lat=${lat}`);
+            const response = await axios.get(`https://izudztw6jk.execute-api.eu-west-1.amazonaws.com/Prod/weather/${geoResponse.data.city}`);
+            setData(response.data);
+            setLoading(false);
         });
-        this.setState({ tasks: newTasks });
     }
 
-    addTasks(name) {
-        const { tasks } = this.state;
-        tasks.push({ name, done: false });
-        this.setState({ tasks });
-    }
+    function OutfitSuggestion({ data }) {
+        let temperature = "";
+        let tenue = "";
+        let icon = "";
+        if (data.temperature < 10) {
+            temperature = "Couvrez vous chaudement il fait froid aujourd'hui !";
+        } else if (data.temperature >= 10 && data.temperature < 20) {
+            temperature = "La température est fraiche habituez vous à un temps plus frais";
+        } else if (data.temperature >= 20) {
+            temperature = "Profitez du soleil";
+        }
 
-
-
-
-
-
-    render() {
-        const { tasks } = this.state;
+        if(data.condition==="sunny"){
+            tenue = "Mettez une casquette et des lunettes de soleil";
+            icon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        className="bi bi-brightness-high" viewBox="0 0 16 16">
+                <path
+                    d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+            </svg>;
+        }
+        else if(data.condition==="rainy"){
+            tenue = "Prenez un parapluie";
+            icon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        className="bi bi-cloud-drizzle" viewBox="0 0 16 16">
+                <path
+                    d="M4.158 12.025a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm-3.5 1.5a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm.747-8.498a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 11H13a3 3 0 0 0 .405-5.973zM8.5 2a4 4 0 0 1 3.976 3.555.5.5 0 0 0 .5.445H13a2 2 0 0 1 0 4H3.5a2.5 2.5 0 1 1 .605-4.926.5.5 0 0 0 .596-.329A4.002 4.002 0 0 1 8.5 2z"/>
+            </svg>;
+        }
+        else if(data.condition==="cloudy"){
+            tenue = "Mettez un pull";
+            icon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        className="bi bi-cloud" viewBox="0 0 16 16">
+                <path
+                    d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
+            </svg>;
+        }
+        else if(data.condition==="windy"){
+            tenue = "Prenez un coupe-vent";
+            icon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        className="bi bi-wind" viewBox="0 0 16 16">
+                <path
+                    d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5zm-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2zM0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5z"/>
+            </svg>;
+        }
+        else if(data.condition==="stormy"){
+            tenue = "Prenez de quoi vous abriter";
+            icon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        className="bi bi-cloud-lightning-rain" viewBox="0 0 16 16">
+                <path
+                    d="M2.658 11.026a.5.5 0 0 1 .316.632l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.316zm9.5 0a.5.5 0 0 1 .316.632l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.316zm-7.5 1.5a.5.5 0 0 1 .316.632l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.316zm9.5 0a.5.5 0 0 1 .316.632l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.316zm-.753-8.499a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 10H13a3 3 0 0 0 .405-5.973zM8.5 1a4 4 0 0 1 3.976 3.555.5.5 0 0 0 .5.445H13a2 2 0 0 1 0 4H3.5a2.5 2.5 0 1 1 .605-4.926.5.5 0 0 0 .596-.329A4.002 4.002 0 0 1 8.5 1zM7.053 11.276A.5.5 0 0 1 7.5 11h1a.5.5 0 0 1 .474.658l-.28.842H9.5a.5.5 0 0 1 .39.812l-2 2.5a.5.5 0 0 1-.875-.433L7.36 14H6.5a.5.5 0 0 1-.447-.724l1-2z"/>
+            </svg>;
+        }
 
         return (
-            <div className="App">
-                <header className="App-header">
-                    <h1>To do list</h1>
-
-                    <ul>
-                        {tasks.map(task => (
-                            <li style={{ textDecoration: task.done ? 'line-through' : 'none' }}>
-                                <input type="checkbox" checked={task.done} onChange={() => this.handleChange(task.name)} />
-                                {task.name}
-                            </li>
-                        ))}
-                    </ul>
-                    <input
-                        type="text"
-                        onKeyPress={event => {
-                            if (event.key === 'Enter') {
-                                this.addTasks(event.target.value);
-                                event.target.value = '';
-                            }
-                        }}
-
-
-
-                        onChange={event => {
-                            this.newTask = event.target.value;
-                        }}
-                    />
-                    <br/>
-                    <button onClick={() => this.addTasks(this.newTask)}>Add</button>
-
-                </header>
+            <div>
+                <div className="icon">{icon}</div>
+                <h2>Conseils pour sortir</h2>
+                <p>{temperature}</p>
+                <p>{tenue}</p>
             </div>
         );
     }
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+    return (
+        <div style={{ margin: "0 auto", maxWidth: 600 }} className="container">
+                <h1 className="title">Weather</h1>
+                <div className="weather-info">
+                    <p className="city">City: {data.city}</p>
+                    <p className="temperature">Temperature: {data.temperature} °C</p>
+                    <p className="condition">Condition: {data.condition}</p>
+                </div>
+                <OutfitSuggestion data={data} />
+        </div>
+    );
 }
 
-
-
 export default App;
+
